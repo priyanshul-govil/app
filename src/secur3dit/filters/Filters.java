cbe608075219278e6f475f19c75e371382076aee
viveknathani <viveknathani2402@gmail.com>
2021-04-10 19:16:19 +0530

    feat: Rotation using math that became necessary
    Image needs to be rotated with a user-specified angle in degrees. There
    are two slightly shorter methods to do this. They are:
    (i) mapping source pixels to destination pixels and
    (ii) deriving destination pixels from source pixels.
    Method (i) does not produce accurate results in many cases because of
    sine and cosine values getting rounded up which leads to a wave-like
    pattern in the result. Method (ii) produces accurate but grainy images.
    So we build up on top of method (ii)'s accuracy and bring in binary
    interpolation.

--- src/secur3dit/filters/Filters.java
+++ src/secur3dit/filters/Filters.java
@@ -1,10 +1,11 @@
-/*
+/**
  * Filters.java 
  * Contains various filters that would be used by the main desktop application.
  */
 
 package secur3dit.filters;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 
 public class Filters {
@@ -29,6 +30,65 @@ public class Filters {
                     result.setRGB(j, i, result.getRGB(j, height - i - 1));
                     result.setRGB(j, height - i - 1, temp);
                 }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * @param angle: floating-point degree, anti-clockwise
+     * Produces a clipped image rotated by angle degrees,
+     * by treating the image as a rectangle on a cartesian plane
+     * and using bilinear interpolation to find the pixel value at every co-ordinate that 
+     * will not have a black color after rotation. For more information about the math involved, 
+     * visit [https://en.wikipedia.org/wiki/Bilinear_interpolation]
+     */
+    public static BufferedImage rotate(BufferedImage image, double angle) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        int imageType = BufferedImage.TYPE_INT_RGB;
+
+        if (image.isAlphaPremultiplied()) {
+            imageType = BufferedImage.TYPE_INT_ARGB;
+        }
+
+        BufferedImage result = new BufferedImage(width, height, imageType);
+
+        angle = Helpers.toRadians(angle);
+        int centreX = width / 2;
+        int centreY = height / 2;
+
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                result.setRGB(j, i, Color.black.getRGB());
+                int[] points = Helpers.toCartesian(j, i, centreX, centreY);
+                if (points[0] == 0 && points[1] == 0) {
+                    continue;
+                }
+
+                double[] polarData = Helpers.toPolar(points[0], points[1]);
+                polarData[1] -= angle;
+                double[] cartesianPoints = Helpers.toCartesian(polarData);
+                double[] rasterPoints = Helpers.toRaster(cartesianPoints, centreX, centreY);
+                int[] floored = Helpers.floorPoints(rasterPoints);
+                int[] ceiled = Helpers.ceilPoints(rasterPoints);
+
+                if (Helpers.outOfBounds(floored[0], width) || 
+                    Helpers.outOfBounds(floored[1], height) || 
+                    Helpers.outOfBounds(ceiled[0], width) || 
+                    Helpers.outOfBounds(ceiled[1], height)) {
+                        continue;
+                }
+                
+                double deltaX = rasterPoints[0] - (double)floored[0];
+                double deltaY = rasterPoints[1] - (double)floored[1];
+                Color topLeft = new Color(image.getRGB(floored[0], floored[1]));
+                Color topRight = new Color(image.getRGB(ceiled[0], floored[1]));
+                Color bottomLeft = new Color(image.getRGB(floored[0], ceiled[1]));
+                Color bottomRight = new Color(image.getRGB(ceiled[0], ceiled[1]));
+                int finalColor = Helpers.bilinearInterpolation(topLeft, topRight, bottomLeft, 
+                    bottomRight, deltaX, deltaY);
+                result.setRGB(j, i, finalColor);
             }
         }
         return result;
--- src/secur3dit/filters/Helpers.java
+++ src/secur3dit/filters/Helpers.java
@@ -5,6 +5,7 @@
 
 package secur3dit.filters;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.WritableRaster;
@@ -15,5 +16,104 @@ public class Helpers {
         boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
         WritableRaster raster = image.copyData(null);
         return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
+    }
+
+    static double toRadians(double degreeAngle) {
+        double result = (degreeAngle * Math.PI) / 180.0;
+        return result;
+    }
+
+    static int[] toCartesian(int rasterX, int rasterY, int centreX, int centreY) {
+        int points[] = new int[2];
+        points[0] = rasterX - centreX;
+        points[1] = centreY - rasterY;
+        return points;
+    }
+
+    static double[] toPolar(int cartesianX, int cartesianY) {
+        double data[] = new double[2];
+        data[0] = Math.sqrt((cartesianX * cartesianX) + (cartesianY * cartesianY));
+        if (cartesianX == 0) {
+            if (cartesianY < 0) {
+                data[1] = 1.5 * Math.PI;
+            }
+            else {
+                data[1] = 0.5 * Math.PI;
+            }
+        }
+        else {
+            data[1] = Math.atan2((double)cartesianY, (double)cartesianX);
+        }
+        return data;
+    }
+
+    static double[] toCartesian(double[] polarData) {
+        double[] points = new double[2];
+        points[0] = polarData[0] * Math.cos(polarData[1]);
+        points[1] = polarData[0] * Math.sin(polarData[1]);
+        return points;
+    }
+
+    static double[] toRaster(double[] cartesianPoints, int centreX, int centreY) {
+        double[] points = new double[2];
+        points[0] = cartesianPoints[0] + (double)centreX;
+        points[1] = (double)centreY - cartesianPoints[1];
+        return points;
+    }
+
+    static int[] floorPoints(double[] rasterPoints) {
+        int[] floored = new int[2];
+        floored[0] = (int)Math.floor(rasterPoints[0]);
+        floored[1] = (int)Math.floor(rasterPoints[1]);
+        return floored;
+    }
+
+    static int[] ceilPoints(double[] rasterPoints) {
+        int[] ceiled = new int[2];
+        ceiled[0] = (int)Math.ceil(rasterPoints[0]);
+        ceiled[1] = (int)Math.ceil(rasterPoints[1]);
+        return ceiled;
+    }
+
+    static boolean outOfBounds(int givenIndex, int length) {
+        return (givenIndex < 0 || givenIndex >= length);
+    }
+
+    static double linearInterpolation(double left, double delta, double right) {
+        double result = (1 - delta) * left + (delta) * right;
+        return result;
+    }
+
+    static int truncateIfNeeded(int pixel) {
+        if (pixel < 0) {
+            return 0;
+        }
+        if (pixel > 255) {
+            return 255;
+        }
+        return pixel;
+    }
+
+    static int bilinearInterpolation(Color topLeft, Color topRight, 
+                                Color bottomLeft, Color bottomRight, 
+                                double deltaX, double deltaY) {
+
+        double topR = linearInterpolation(topLeft.getRed(), deltaX, topRight.getRed());
+        double topG = linearInterpolation(topLeft.getGreen(), deltaX, topRight.getGreen());
+        double topB = linearInterpolation(topLeft.getBlue(), deltaX, topRight.getBlue());
+
+        double bottomR = linearInterpolation(bottomLeft.getRed(), deltaX, bottomRight.getRed());
+        double bottomG = linearInterpolation(bottomLeft.getGreen(), deltaX, bottomRight.getGreen());
+        double bottomB = linearInterpolation(bottomLeft.getBlue(), deltaX, bottomRight.getBlue());
+
+        int red = (int)Math.round(linearInterpolation(topR, deltaY, bottomR));
+        int green = (int)Math.round(linearInterpolation(topG, deltaY, bottomG));
+        int blue = (int)Math.round(linearInterpolation(topB, deltaY, bottomB));
+
+        red = truncateIfNeeded(red);
+        green = truncateIfNeeded(green);
+        blue = truncateIfNeeded(blue);
+        Color finalColor = new Color(red, green, blue);
+        return finalColor.getRGB();
     }
 };
