6a44f27dc85c0265b03aaed7903666a5a1516207
namannihal <naman.nihal02@gmail.com>
2021-04-13 11:15:35 +0530

    style : Indentation changes as per coding style guide

--- src/secur3dit/filters/Filters.java
+++ src/secur3dit/filters/Filters.java
@@ -27,7 +27,8 @@ public class Filters {
                     result.setRGB(width - j - 1, i, temp);
                 }
             }
-        } else {
+        } 
+        else {
             for (int j = 0; j < width; ++j) {
                 for (int i = 0; i < height / 2; ++i) {
                     int temp = result.getRGB(j, i);
@@ -40,13 +41,12 @@ public class Filters {
     }
 
     /**
-     * @param angle: floating-point degree, anti-clockwise Produces a clipped image
-     *               rotated by angle degrees, by treating the image as a rectangle
-     *               on a cartesian plane and using bilinear interpolation to find
-     *               the pixel value at every co-ordinate that will not have a black
-     *               color after rotation. For more information about the math
-     *               involved, visit
-     *               [https://en.wikipedia.org/wiki/Bilinear_interpolation]
+     * @param angle: floating-point degree, anti-clockwise 
+     * Produces a clipped image rotated by angle degrees,
+     * by treating the image as a rectangle on a cartesian plane 
+     * and using bilinear interpolation to find the pixel value at every co-ordinate that 
+     * will not have a black color after rotation. For more information about the math involved,
+     * visit [https://en.wikipedia.org/wiki/Bilinear_interpolation]
      */
     public static BufferedImage rotate(BufferedImage image, double angle) {
         int width = image.getWidth();
@@ -77,10 +77,12 @@ public class Filters {
                 double[] rasterPoints = Helpers.toRaster(cartesianPoints, centreX, centreY);
                 int[] floored = Helpers.floorPoints(rasterPoints);
                 int[] ceiled = Helpers.ceilPoints(rasterPoints);
-
-                if (Helpers.outOfBounds(floored[0], width) || Helpers.outOfBounds(floored[1], height)
-                        || Helpers.outOfBounds(ceiled[0], width) || Helpers.outOfBounds(ceiled[1], height)) {
-                    continue;
+                
+                if (Helpers.outOfBounds(floored[0], width) || 
+                    Helpers.outOfBounds(floored[1], height) || 
+                    Helpers.outOfBounds(ceiled[0], width) || 
+                    Helpers.outOfBounds(ceiled[1], height)) {
+                        continue;
                 }
 
                 double deltaX = rasterPoints[0] - (double) floored[0];
@@ -89,8 +91,8 @@ public class Filters {
                 Color topRight = new Color(image.getRGB(ceiled[0], floored[1]));
                 Color bottomLeft = new Color(image.getRGB(floored[0], ceiled[1]));
                 Color bottomRight = new Color(image.getRGB(ceiled[0], ceiled[1]));
-                int finalColor = Helpers.bilinearInterpolation(topLeft, topRight, bottomLeft, bottomRight, deltaX,
-                        deltaY);
+                int finalColor = Helpers.bilinearInterpolation(topLeft, topRight, bottomLeft,
+                    bottomRight, deltaX, deltaY);
                 result.setRGB(j, i, finalColor);
             }
         }
@@ -127,8 +129,9 @@ public class Filters {
                         int indexI = i + m - 1;
                         int indexJ = j + n - 1;
 
-                        if (Helpers.outOfBounds(indexI, height) || Helpers.outOfBounds(indexJ, width)) {
-                            continue;
+                        if (Helpers.outOfBounds(indexI, height) || 
+                            Helpers.outOfBounds(indexJ, width)) {
+                                continue;
                         }
 
                         Color color = new Color(image.getRGB(indexJ, indexI));
@@ -143,8 +146,8 @@ public class Filters {
                 }
 
                 for (int k = 0; k < 3; ++k) {
-                    sobelXY[k] = (int) Math
-                            .round(Math.sqrt((sobelSumX[k] * sobelSumX[k] + sobelSumY[k] * sobelSumY[k])));
+                    sobelXY[k] = (int) Math.round(Math.sqrt((sobelSumX[k] * sobelSumX[k] + 
+                                                             sobelSumY[k] * sobelSumY[k])));
                     sobelXY[k] = Helpers.truncateIfNeeded(sobelXY[k]);
                 }
 
@@ -247,6 +250,7 @@ public class Filters {
     }
 
     public static BufferedImage addWatermark(BufferedImage image, String watermarkText) {
+        
         BufferedImage img = Helpers.deepCopy(image);
         Graphics2D graphics = (Graphics2D) img.getGraphics();
 
