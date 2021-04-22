9c2bcb884a9949fb571a48ea5592e31c818ea7e6
viveknathani <viveknathani2402@gmail.com>
2021-04-22 18:22:36 +0530

    refactor: Unsuitable naming and computing
    'pixelRatio' is changed to 'pixelWidth' to provide a name that suits
    better. Average calculation could be better by using three more
    variables to handle the summation.

--- src/secur3dit/filters/Filters.java
+++ src/secur3dit/filters/Filters.java
@@ -446,13 +446,13 @@ public class Filters {
     
     /**
      * Takes an image and returns a pixelated version of it.
-     * It needs an extra parameter, pixelRatio which can take values
+     * It needs an extra parameter, pixelWidth which can take values
      * in the range [1, min(height - 1, width - 1)]. A square window of side
-     * length pixelRatio slides through the image, jumping at a length of 
-     * pixelRatio after each iteration. In each slide, it sets the R,G,B values 
+     * length pixelWidth slides through the image, jumping at a length of 
+     * pixelWidth after each iteration. In each slide, it sets the R,G,B values 
      * of every pixel to the average values in that window.
      */
-    public static BufferedImage pixelate(BufferedImage image, int pixelRatio) {
+    public static BufferedImage pixelate(BufferedImage image, int pixelWidth) {
         
         BufferedImage result = Helpers.deepCopy(image);
         int width = image.getWidth();
@@ -463,41 +463,41 @@ public class Filters {
         int avgGreen = 0;
         int avgBlue = 0;
         
-        for (int i = 0; i < height; i += pixelRatio) {
-            for (int j = 0; j < width; j += pixelRatio) {
+        for (int i = 0; i < height; i += pixelWidth) {
+            for (int j = 0; j < width; j += pixelWidth) {
 
                 // Check if the indices are such that we have a distinct submatrix
-                if (i % pixelRatio == 0 && j % pixelRatio == 0) {
+                if (i % pixelWidth == 0 && j % pixelWidth == 0) {
 
-                    avgRed = 0;
-                    avgGreen = 0;
-                    avgBlue = 0;
+                    int totalRed = 0;
+                    int totalGreen = 0;
+                    int totalBlue = 0;
                     int count = 0;
 
                     // Traverse and add the pixel values
-                    for (int y = i; y < i + pixelRatio && y < height; ++y) {
-                        for (int x = j; x < j + pixelRatio && x < width; ++x) {
+                    for (int y = i; y < i + pixelWidth && y < height; ++y) {
+                        for (int x = j; x < j + pixelWidth && x < width; ++x) {
                             Color color = new Color(result.getRGB(x, y));
 
-                            avgRed += color.getRed();
-                            avgGreen += color.getGreen();
-                            avgBlue += color.getBlue();
+                            totalRed += color.getRed();
+                            totalGreen += color.getGreen();
+                            totalBlue += color.getBlue();
                             ++count;
                         }
                     }
 
                     // Compute the average
-                    avgRed /= count;
-                    avgGreen /= count;
-                    avgBlue /= count;
+                    avgRed = totalRed/count;
+                    avgGreen = totalGreen/count;
+                    avgBlue = totalBlue/count;
                 }
 
                 // Get the color made using the R,G,B values
                 Color finalColor = new Color(avgRed, avgGreen, avgBlue);
 
                 // Set all pixels in the matrix to finalColor
-                for (int y = i; y < i + pixelRatio && y < height; ++y) {
-                    for (int x = j; x < j + pixelRatio && x < width; ++x) {
+                for (int y = i; y < i + pixelWidth && y < height; ++y) {
+                    for (int x = j; x < j + pixelWidth && x < width; ++x) {
                         result.setRGB(x, y, finalColor.getRGB());
                     }
                 }
