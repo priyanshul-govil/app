1821f36706d2bdcf2988dd2e5674fd27b490467b
namannihal <naman.nihal02@gmail.com>
2021-05-19 19:35:08 +0530

    refactor: formatting
    Made formatting changes and removed any unnecessary comments

--- src/secur3dit/Main.java
+++ src/secur3dit/Main.java
@@ -12,24 +12,21 @@ import javafx.stage.Stage;
  *
  * @author naman
  */
-public class Main extends Application{
+public class Main extends Application {
     public static Stage stage = null;
+
     public static void main(String[] args) {
-        // TODO code application logic here
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-       Parent root = FXMLLoader.load(getClass().getResource("ui/homepage.fxml"));
-       GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-//       int width = gd.getDisplayMode().getWidth();
-//       int height = gd.getDisplayMode().getHeight();
-       Scene s = new Scene(root);
-       s.getStylesheets().add(getClass().getResource("ui/design.css").toExternalForm());
-       stage.setScene(s);
-       this.stage=stage;
-       stage.show();
-        
+        Parent root = FXMLLoader.load(getClass().getResource("ui/homepage.fxml"));
+        Scene s = new Scene(root);
+        s.getStylesheets().add(getClass().getResource("ui/design.css").toExternalForm());
+        stage.setScene(s);
+        this.stage = stage;
+        stage.show();
+
     }
 }
